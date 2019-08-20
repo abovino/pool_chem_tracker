bc38d0bb7e7198f845a5e761dbb8fa6b3cf8037f
Angelo Bovino <angelo.bovino@outlook.com>
2019-08-20 19:16:56 -0400

    Require dotenv for PORT

--- api/server.js
+++ api/server.js
@@ -1,9 +1,10 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
-
+const PORT = process.env.PORT || 3000;
 app.use(express.json({ extended: true }));
 
 // Routes
 app.use('/api/auth', require('./routes/auth.routes'));
 
-app.listen(3000, (req, res) => console.log('listening'));
+app.listen(PORT, (req, res) => console.log(`Listening on ${PORT}`));
